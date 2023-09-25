7e26ca52969e54c18c0474da185d19e7dc9166a5
Kuan <soonkwan21c@gmail.com>
2023-09-25 19:52:02 +0900

    9/25

--- bj2448.cpp
+++ bj2448.cpp
@@ -15,24 +15,52 @@ void INIT(){
     ios_base::sync_with_stdio(false);
 }
 char** setMem(int n){
-    char** table = new char*[n];
-    for (int i=0; i<n; i++) table[i] = new char[2*n-1];
-    for (int i=0; i<n; i++){
-        for (int j=0; j<2*n-1; j++){
+    char** table = new char*[n+1];
+    for (int i=0; i<=n; i++){
+        table[i] = new char[n*2+1];
+        for (int j=0; j<=2*n; j++){
             table[i][j] = ' ';
         }
     }
     return table;
 }
 void freeMem(char** t, int n){
-    for (int i=0; i<n; i++){
-        delete[] t[i];
-    }
+    for (int i=0; i<=n; i++) delete[] t[i];
     delete[] t;
+}
+void printStar(char** t, int x, int y, int n){
+    if (n==3){
+        // cout << x << " " << y << endl;
+        t[x-2][y] = '*';
+
+        t[x-1][y-1] = '*';
+        t[x-1][y+1] = '*';
+
+        t[x][y-2] = '*';
+        t[x][y-1] = '*';
+        t[x][y] = '*';
+        t[x][y+1] = '*';
+        t[x][y+2] = '*';
+        return;
+    }
+    printStar(t, x-n/2, y, n/2);
+    printStar(t, x, y-n/2, n/2);
+    printStar(t, x, y+n/2, n/2);
+}
+void printTable(char** t, int n){
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=2*n; j++){
+            cout << t[i][j];
+        }
+        cout << "\n";
+    }
 }
 void INPUT(){
     int n; cin >> n;
-    
+    char** table = setMem(n);
+    printStar(table, n, n, n);
+    printTable(table, n);
+    freeMem(table, n);
 }
 
 int main(){
