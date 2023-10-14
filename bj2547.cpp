5460222663440e656217ba941a12afbebf482463
Kuan <soonkwan21c@gmail.com>
2023-10-14 20:03:10 +0900

    10/14

---
+++ bj2547.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+void INIT(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+}
+void INPUT(){
+    int t; cin >> t;
+    while (t--){
+        int people; cin >> people;
+        ll total = 0;
+        for (int i=0; i<people; i++){
+            ll tmp; cin >> tmp;
+            total = (total + tmp) % people;
+        }
+
+        if (total % people == 0){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
+int main(){
+    INIT();
+    INPUT();
+    return 0;
+}
