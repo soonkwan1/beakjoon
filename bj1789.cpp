059af6b086d02aed4e39663bee3bf0681abaa6f0
Kuan <soonkwan21c@gmail.com>
2023-10-20 20:36:51 +0900

    10/20

--- bj1789.cpp
+++ bj1789.cpp
@@ -8,6 +8,7 @@
 #include <map>
 #include <set>
 #define endl "\n"
+#define ll long long
 using namespace std;
 
 void INIT(){
@@ -16,7 +17,23 @@ void INIT(){
     ios_base::sync_with_stdio(false);
 }
 void INPUT(){
-    
+    ll n; cin >> n;
+    ll num = 1;
+    int cnt = 0;
+
+    ll sum = 0;
+    while (true){
+        sum += num;
+        cnt++;
+        if (sum > n){
+            cnt--;
+            break;
+        }
+        num++;
+        
+    }
+
+    cout << cnt << endl;
 }
 
 int main(){
