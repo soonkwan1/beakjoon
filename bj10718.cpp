5ffc0a8388e9b53741c8a9112541e0206fc31d44
Kuan <soonkwan21c@gmail.com>
2023-10-22 18:26:25 +0900

    10/22

---
+++ bj10718.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#define endl "\n"
+using namespace std;
+
+void INIT(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+}
+void INPUT(){
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+}
+
+int main(){
+    INIT();
+    INPUT();
+    return 0;
+}
