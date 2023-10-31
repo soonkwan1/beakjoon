58c8fcf8879e9fea189d59f241e5a50f81552d35
Kuan <88532488+soonkwan1@users.noreply.github.com>
2023-10-31 17:08:03 +0900

    10/31

---
+++ bj17356.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#define endl "\n"
+using namespace std;
+
+void INIT() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+}
+void INPUT() {
+    double a, b; cin >> a >> b;
+    double m = (b - a) / 400;
+    cout.precision(6);
+    cout << 1 / (1 + pow(10, m)) << endl;
+}
+
+int main() {
+    INIT();
+    INPUT();
+    return 0;
+}
