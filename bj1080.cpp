dfcc9904593173f63c3e35011321724fcd65d520
Kuan <soonkwan21c@gmail.com>
2023-11-08 22:09:16 +0900

    11/8

--- bj1080.cpp
+++ bj1080.cpp
@@ -19,12 +19,67 @@ void INIT(){
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 }
+char** getMatrix(int n, int m){
+    char** A = new char*[n];
+    for (int i=0; i<n; i++){
+        A[i] = new char[m];
+        string line; cin >> line;
+        for (int j=0; j<m; j++){
+            A[i][j] = line[j];
+        }
+    }
+    return A;
+}
+void freeMem(char** t, int n){
+    for (int i=0; i<n; i++) delete[] t[i];
+    delete[] t;
+}
+void reverseMatrix(char** t, int x, int y){
+    for (int i=x; i<x+3; i++){
+        for (int j=y; j<y+3; j++){
+            if (t[i][j] == '0') t[i][j] = '1';
+            else t[i][j] = '0';
+        }
+    }
+}
+int isSameMatrix(char** t1, char** t2, int n, int m){
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            if (t1[i][j] != t2[i][j]) return 0;
+        }
+    }
+    return 1;
+}
 void INPUT(){
     int n, m; cin >> n >> m;
-    int** A = new int*[n];
-    for (int i=0; i<n; i++){
-        
+    char** A = getMatrix(n, m);
+    char** B = getMatrix(n, m);
+    
+    if (n < 3 && m < 3){
+        if (isSameMatrix(A, B, n, m)){
+            cout << 0 << endl;
+        }
+        else{
+            cout << -1 << endl;
+        }
     }
+    else{
+        int ans = 0;
+        for (int i=0; i<=n-3; i++){
+            for (int j=0; j<=m-3; j++){
+                if (A[i][j] != B[i][j]) {
+                    reverseMatrix(A, i, j);
+                    ans++;
+                }
+            }
+        }
+        if (isSameMatrix(A, B, n, m)) cout << ans << endl;
+        else cout << -1 << endl;
+    }
+
+
+    freeMem(A, n);
+    freeMem(B, n);
 }
 
 int main(){
