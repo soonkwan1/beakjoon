6372c337a450de5466d7c85589beaf1d92b5d13e
Kuan <88532488+soonkwan1@users.noreply.github.com>
2023-11-22 13:01:14 +0900

    11/22

---
+++ bj9653.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+
+void INIT() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+}
+void INPUT() {
+    cout << "    8888888888  888    88888" << endl;
+    cout << "   88     88   88 88   88  88" << endl;
+    cout << "    8888  88  88   88  88888" << endl;
+    cout << "       88 88 888888888 88   88" << endl;
+    cout << "88888888  88 88     88 88    888888" << endl;
+    cout << "" << endl;
+    cout << "88  88  88   888    88888    888888" << endl;
+    cout << "88  88  88  88 88   88  88  88" << endl;
+    cout << "88 8888 88 88   88  88888    8888" << endl;
+    cout << " 888  888 888888888 88  88      88" << endl;
+    cout << "  88  88  88     88 88   88888888" << endl;
+}
+
+int main() {
+    INIT();
+    INPUT();
+    return 0;
+}
+/*
+    8888888888  888    88888
+   88     88   88 88   88  88
+    8888  88  88   88  88888
+       88 88 888888888 88   88
+88888888  88 88     88 88    888888
+
+88  88  88   888    88888    888888
+88  88  88  88 88   88  88  88
+88 8888 88 88   88  88888    8888
+ 888  888 888888888 88  88      88
+  88  88  88     88 88   88888888
+*/
