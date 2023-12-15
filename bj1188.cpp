235b01f5464325d89870e2345a26ad7865a9b023
Kuan <soonkwan21c@gmail.com>
2023-12-15 17:05:16 +0900

    12/15

---
+++ bj1188.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fse(A,B,C) for (int i=A; i<B; i+=C)
+using namespace std;
+
+void INIT(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+}
+void INPUT(){
+    int n, m; cin >> n >> m;
+    if (n % m == 0) {
+        cout << 0 << endl;
+        return;
+    }
+
+    
+}
+
+int main(){
+    INIT();
+    INPUT();
+    return 0;
+}
