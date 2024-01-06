d67fe3be6c4999dc9e4c675c1e167d51a1b65f7d
Kuan <88532488+soonkwan1@users.noreply.github.com>
2024-01-06 17:46:18 +0900

    24/1/6

---
+++ bj2446.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n - 1; i++) {
+
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+
+		for (int k = 1; k <= (n * 2 - (i * 2 + 1)); k++) {
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; i++) {
+
+		for (int j = 1; j <= n - i - 1; j++) {
+			cout << " ";
+		}
+
+		for (int k = 1; k <= (i * 2 + 1); k++) {
+			cout << "*";
+		}
+        
+		cout << endl;
+	}
+
+}
