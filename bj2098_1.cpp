8973a09ef70390e7b0e4b99a5f537d50c39aa388
Kuan <soonkwan21c@gmail.com>
2024-02-13 21:01:56 +0900

    24/2/13

--- bj2098_1.cpp
+++ bj2098_1.cpp
@@ -17,20 +17,20 @@ void INIT(){
     ios_base::sync_with_stdio(false);
 }
 int** setMem(int n){
-    int** tmp = new int*[n+1];
-    for (int i=0; i<=n; i++){
-        tmp[i] = new int[n+1];
+    int** tmp = new int*[n];
+    for (int i=0; i<n; i++){
+        tmp[i] = new int[n];
     }
 
-    for (int i=1; i<=n; i++){
-        for (int j=1; j<=n; j++){
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
             cin >> tmp[i][j];
         }
     }
     return tmp;
 }
 void freeMem(int** t, int n){
-    for (int i=0; i<=n; i++) delete[] t[i];
+    for (int i=0; i<n; i++) delete[] t[i];
     delete[] t;
 }
 void printTable(int** dp, int n){
@@ -42,42 +42,46 @@ void printTable(int** dp, int n){
         cout << endl;
     }
 }
-int dfs(int curr, int prev, int start, int n, int** dp, int** table){
-    if (curr == pow(2, n) - 1){
-        if (table[prev][start]){
-            return table[prev][start];
+int dfs(int curr_bit, int node, int n, int** dp, int** table){
+    if (curr_bit == (1 << n) - 1){
+        if (table[node][0] == 0){
+            return INF;    
         }
-        return INF;
+        return table[node][0];
     }
 
-    
-    for (int i=1; i<=n; i++){
-        if ((curr & (1 << (i-1)))) continue;
-        if (!table[prev][i]) continue;
-        int new_bit = (curr | 1 << (i-1));
-
-        dp[i][new_bit] = min(dp[i][new_bit], dfs(new_bit, i, start, n, dp, table));
+    if (dp[node][curr_bit] != -1){
+        return dp[node][curr_bit];
     }
-    
+
+    dp[node][curr_bit] = INF;
+    for (int i=0; i<n; i++){
+        // road existence check
+        if (table[node][i] == 0) continue;
+        // visited node check
+        if ((curr_bit & (1 << i)) == (1 << i)) continue;
+
+        // going bit
+        int new_bit = (curr_bit | 1 << i);
+
+        dp[node][curr_bit] = min(dp[node][curr_bit], table[node][i] +  dfs(new_bit, i, n, dp, table));
+    }
+    return dp[node][curr_bit];
 }
 void INPUT(){
     int n; cin >> n;
     int** table = setMem(n);
 
-    int** dp = new int*[n+1];
-    for (int i=0; i<=n; i++){
-        dp[i] = new int[int(pow(2, n))];
-        fill_n(dp[i], int(pow(2,n)), 1e9);
-        dp[i][0] = 0;
+    int** dp = new int*[n];
+    for (int i=0; i<n; i++){
+        dp[i] = new int[1 << n];
+        fill_n(dp[i], 1 << n, -1);
     }
     
-    for (int i=1; i<=n; i++){
-        int start_bit = 1 << (i-1);
-        dfs(start_bit, i, i, n, dp, table);
-    }
+    cout << dfs(0, 1, n, dp, table) << endl;
 
-    printTable(dp, n);
-    
+    // printTable(dp, n);
+
     freeMem(table, n);
     freeMem(dp, n);
 }
