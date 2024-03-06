b3fc724917044b99e789a1a107e35389653f7d03
Kuan <88532488+soonkwan1@users.noreply.github.com>
2024-03-06 17:33:20 +0900

    24/3/6

--- bj9205.cpp
+++ bj9205.cpp
@@ -19,14 +19,28 @@ void SOLUTION() {
 bool** getVisit(int n) {
     bool** tmp = new bool* [n];
     for (int i = 0; i < n; i++) {
-        tmp[i]
+        tmp[i] = new bool[n];
+        fill_n(tmp[i], n, false);
     }
+    return tmp;
 }
-void bfs(vector<int>* graph, vector<int> v) {
+void freeVisit(bool** t, int n) {
+    for (int i = 0; i < n; i++) delete[] t[i];
+    delete[] t;
+}
+void bfs(vector<int>* graph, vector<pair<int,int>> v, int n) {
+    int start_x = v[0].first, start_y = v[0].second;
     deque<pair<int, int>> dq;
     dq.push_back(make_pair(v[0].first, v[0].second));
 
-    
+    bool** visited = getVisit(n);
+    visited[start_x][start_y] = true;
+
+    while (!dq.empty()) {
+
+    }
+
+    freeVisit(visited, n);
 }
 void INPUT() {
     int t; cin >> t;
