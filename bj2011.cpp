e7ed365d4ddcd779389a51c99e71704faffe0459
Kuan <soonkwan21c@gmail.com>
2024-03-11 23:38:49 +0900

    24/3/11

--- bj2011.cpp
+++ bj2011.cpp
@@ -14,64 +14,34 @@
 #define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
 using namespace std;
 
-int** dp;
-void getDP(){
-    dp = new int*[5001];
-    for (int i=0; i<=5000; i++){
-        dp[i] = new int[10];
-        fill_n(dp[i], 10, 0);
-    }
-}
-void freeDP(){
-    for (int i=0; i<=5000; i++) delete dp[i];
-    delete[] dp;
-}
-void printDP(){
-    for (int i=1; i<5; i++){
-        for (int j=1; j<10; j++){
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+int ndp[5001];
 void INPUT(){
-    
-}
-void SOLUTION(){
-    getDP();
-
-    int start1 = 0, start2 = 0;
-    for (int i=1; i<10; i++){
-        dp[1][i] = 1;
-        if (i <= 6) dp[2][i] = 11;
-        else dp[2][i] = 9;
-        start1 += dp[1][i];
-        start2 += dp[2][i];
-    }
-
-    int md = 1000000;
-    for (int i=3; i<=5000; i++){
-        int tmp = 0;
-        for (int j=1; j<10; j++){
-            if (j <= 6){
-                dp[i][j] = (start1 % md + 18 + start2 % md) % md;
-            }
-            else{
-                dp[i][j] = start2 % md;
-            }
-            tmp = (tmp % md + dp[i][j]) % md;
-        }
-        start1 = start2;
-        start2 = tmp;
-    }
-    printDP();
+    fill_n(ndp, 5001, 0);
+    vector<int> v; v.push_back(0);
     string line; cin >> line;
-    cout << dp[line.size()][int(line[line.size()-1]) - 48] << endl;
+    int nsize = line.size();
+    for (int i=0; i<nsize; i++) {
+        v.push_back(int(line[i]) - 48);
+    }
 
-    freeDP();
+    if (v[1] == 0){
+        cout << 0 << endl;
+        return;
+    }
+
+    ndp[0] = ndp[1] = 1;
+    int md = 1000000;
+    for (int i=2; i<=nsize; i++){
+        if (v[i] != 0) ndp[i] = ndp[i-1];
+
+        int tmp = v[i - 1] * 10 + v[i];
+        if (tmp >= 10 && tmp <= 26) ndp[i] = (ndp[i] + ndp[i-2]) % md;
+    }
+    cout << ndp[nsize] << endl;
 }
 int main(){
     fastio;
-    SOLUTION();
+    INPUT();
+    // SOLUTION();
     return 0;
 }
