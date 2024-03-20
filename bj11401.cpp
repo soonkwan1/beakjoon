7ff66d8f15c0577e86514a1f8e742a609b784322
Kuan <soonkwan21c@gmail.com>
2024-03-20 22:12:55 +0900

    24/3/20

--- bj11401.cpp
+++ bj11401.cpp
@@ -20,13 +20,32 @@ void INPUT(){
     cin >> n >> k;
 }
 void SOLUTION(){
-    ll* num = new ll[4000001];
-    fill_n(num, 4000001, 0);
-    num[0] = num[1] = 1;
-    for (int i=2; i<=4000000; i++){
-        num[i] = (num[i-1] % MOD * i % MOD) % MOD;
+    // ll* num = new ll[4000001];
+    // fill_n(num, 4000001, 0);
+    // num[0] = num[1] = 1;
+    // for (int i=2; i<=4000000; i++){
+    //     num[i] = (num[i-1] % MOD * i % MOD) % MOD;
+    // }
+    // cout << (num[n] / num[k]) / num[n - k] << endl;
+
+    deque<ll> v;
+    for (int i=k+1; i<=n; i++){
+        v.push_back(i);
     }
-    cout << (num[n] / num[k]) / num[n - k] << endl;
+    for (int i=2; i<=n-k; i++){
+        for (int j=0; j<v.size(); j++){
+            if (v[j] % i == 0){
+                v[j] = v[j] / i == 0 ? 1 : v[j] / i;
+            }
+        }
+    }
+
+    ll ans = 1;
+    for (int i=0; i<v.size(); i++){
+        if (v[i] == 0) continue;
+        ans = (ans % MOD * v[i] % MOD) % MOD;
+    }
+    cout << ans << endl;
 }
 int main(){
     fastio;
