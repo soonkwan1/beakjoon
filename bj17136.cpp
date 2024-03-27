21cd9eda9c7aa5472f845f087e03fa49a33cc40e
Kuan <soonkwan21c@gmail.com>
2024-03-27 22:12:37 +0900

    24/3/27

--- bj17136.cpp
+++ bj17136.cpp
@@ -16,6 +16,7 @@ using namespace std;
 
 int table[10][10];
 int paperList[6] = {0, 5, 5, 5, 5, 5};
+int ans = int(1e9);
 void INPUT(){
     for (int i=0; i<10; i++){
         for (int j=0; j<10; j++){
@@ -49,25 +50,72 @@ int checkPaper(int x, int y, int s){
     paperList[s]--;
     return 1;
 }
-void SOLUTION(){
-    for (int k=5; k>=1; k--){
-        for (int i=0; i<=10-k; i++){
-            for (int j=0; j<=10-k; j++){
-                if (table[i][j] == 0) continue;
-                
-                int res = checkPaper(i, j, k);
-                if (res == -1) {
-                    cout << -1 << endl;
-                    return;
-                }
-            }
+pair<int,int> findOne(){
+    for (int i=0; i<10; i++){
+        for (int j=0; j<10; j++){
+            if (table[i][j] == 1) return make_pair(i, j);
         }
     }
-    int ans = 25;
-    for (int i=1; i<=5; i++){
-        ans -= paperList[i];
+    return make_pair(10, 10);
+}
+
+bool hasZero(int x, int y, int k){
+    for (int i=x; i<x+k; i++){
+        for (int j=y; j<y+k; j++){
+            if (table[i][j] == 0) return true;
+        }
     }
-    cout << ans << endl;
+    return false;
+}
+bool hasOne(){
+    for (int i=0; i<10; i++){
+        for (int j=0; j<10; j++){
+            if (table[i][j] == 1) return true;
+        }
+    }
+    return false;
+}
+void transferOne(int x, int y, int k){
+    for (int i=x; i<x+k; i++){
+        for (int j=y; j<y+k; j++){
+            table[i][j] = 1;
+        }
+    }
+}
+void transferZero(int x, int y, int k){
+    for (int i=x; i<x+k; i++){
+        for (int j=y; j<y+k; j++){
+            table[i][j] = 0;
+        }
+    }
+}
+void dfs(int x, int y, int depth){
+    if (!hasOne()){
+        ans = min(ans, depth);
+        return;
+    }
+
+    for (int k=5; k>=1; k--){
+        if (x + k  > 10 || y + k > 10 || !paperList[k]) continue;
+        
+        bool chk = hasZero(x, y, k);
+        if (!chk){
+            transferZero(x, y, k);
+            paperList[k]--;
+
+            pair<int,int> n_par = findOne();
+            dfs(n_par.first, n_par.second, depth + 1);
+
+            transferOne(x, y, k);
+            paperList[k]++;
+        }
+    }
+}
+void SOLUTION(){
+    pair<int,int> start = findOne();
+    dfs(start.first, start.second, 0);
+    if (ans == int(1e9)) cout << -1 << endl;
+    else cout << ans << endl;
 }
 int main(){
     fastio;
