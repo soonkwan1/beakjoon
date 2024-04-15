feac8d78948011a9e82d0b22ec39464713e7e533
Kuan <88532488+soonkwan1@users.noreply.github.com>
2024-04-15 22:11:19 +0900

    24/4/15

---
+++ bj29751.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double w, h;
+    cin >> w >> h;
+    cout << fixed;
+    cout.precision(1);
+    cout << w * h / 2 << endl;
+    return 0;
+}
