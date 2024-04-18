7d827fa11a5aa9eae149c1a1951baf1fb80a59e1
Kuan <soonkwan21c@gmail.com>
2024-04-18 19:45:00 +0900

    24/4/18

--- bj15651.cpp
+++ bj15651.cpp
@@ -14,11 +14,34 @@
 #define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
 using namespace std;
 
+int n, m;
+void dfs(int k, vector<int> v){
+    if (v.size() == m){
+        for (int i=0; i<v.size(); i++){
+            cout << v[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for (int i=1; i<=n; i++){
+        v.push_back(i);
+        dfs(i, v);
+        v.pop_back();
+    }
+}
 void INPUT(){
+    cin >> n >> m;
     
+    vector<int> v;
+    for (int i=1; i<=n; i++){
+        v.push_back(i);
+        dfs(i, v);
+        v.pop_back();
+    }
 }
 void SOLUTION(){
-    
+
 }
 int main(){
     fastio;
