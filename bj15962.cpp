ceaa46b5a9e1b2cd4b063e81cb2d21960d873f03
Kuan <88532488+soonkwan1@users.noreply.github.com>
2024-05-24 20:31:14 +0900

    24/5/24

---
+++ bj15962.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+
+void INPUT() {
+    cout << "파이팅!!" << endl;
+}
+
+int main() {
+    fastio;
+    INPUT();
+    return 0;
+}
