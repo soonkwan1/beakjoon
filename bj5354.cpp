24502d62256f69980f5f692179840e5452542bca
Kuan <88532488+soonkwan1@users.noreply.github.com>
2024-06-02 23:41:55 +0900

    24/6/2

---
+++ bj5354.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int box;
+        cin >> box;
+        for (int i = 0; i < box; i++)
+        {
+            for (int j = 0; j < box; j++)
+            {
+                if (i != 0 && i != box - 1 && j != 0 && j != box - 1)
+                    cout << "J";
+                else
+                    cout << "#";
+            }
+            cout << '\n';
+        }
+        cout << '\n';
+    }
+}
