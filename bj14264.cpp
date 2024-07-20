d04e0541412194a8b143a4aa505021cb0bc0e52d
Kuan <88532488+soonkwan1@users.noreply.github.com>
2024-07-20 21:24:34 +0900

    24/7/20

---
+++ bj14264.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+
+void INPUT() {
+    int n; cin >> n;
+    
+    cout.precision(9);
+    cout << fixed;
+
+    cout << sqrt(3) * n * n / 4 << endl;
+}
+
+int main() {
+    fastio;
+    INPUT();
+    return 0;
+}
