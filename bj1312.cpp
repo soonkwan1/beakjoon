48ca1a8ec2aefc90246b7e7db5457e9262033e43
Kuan <soonkwan21c@gmail.com>
2024-09-11 23:40:11 +0900

    24/9/11

--- bj1312.cpp
+++ bj1312.cpp
@@ -20,13 +20,20 @@ void INIT(){
     ios_base::sync_with_stdio(false);
 }
 void INPUT(){
-    double a, b; cin >> a >> b;
-    int k = a / b;
-    int n; cin >> n;
-    cout.precision(n - k);
+    int a, b, n; cin >> a >> b >> n;
+    
+    int res = 0;
+    if (a % b == 0) cout << 0 << endl;
+    else{
+        if (a > b) a = a % b;
 
-    double c = a / b;
-    cout << c << endl;
+        for (int i=0; i<n; i++){
+            a *= 10;
+            res = (int)(a / b);
+            a = a % b;
+        }
+        cout << res << endl;
+    }
 }
 
 int main(){
