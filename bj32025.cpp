559f7d29ae9cb1fa101c9f510d22eef6c82e3978
Kuan <soonkwan21c@gmail.com>
2025-02-02 20:00:45 +0900

    25/2/2

---
+++ bj32025.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+using namespace std;
+
+void INPUT(){
+    int a, b; cin >> a >> b;
+    if (a > b) swap(a, b);
+    cout << a * 50 << endl;
+}
+void SOLUTION(){
+
+}
+int main(){
+    fastio;
+    INPUT();
+    SOLUTION();
+    return 0;
+}
