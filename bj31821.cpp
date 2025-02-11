f7bd73d60277fbb4c356f595813f97014df9423e
Kuan <soonkwan21c@gmail.com>
2025-02-11 19:11:13 +0900

    25/2/11

---
+++ bj31821.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+using namespace std;
+
+void INPUT(){
+    int n; cin >> n;
+    vector<int> v(n+1);
+    for (int i=1; i<=n; i++) cin >> v[i];
+
+    int m; cin >> m;
+    int total = 0;
+    for (int i=0; i<m; i++){
+        int tmp; cin >> tmp;
+        total += v[tmp];
+    }
+    cout << total << endl;
+}
+void SOLUTION(){
+
+}
+int main(){
+    fastio;
+    INPUT();
+    SOLUTION();
+    return 0;
+}
