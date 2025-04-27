f811f7f0166652e88a71193671d54e6529f8984b
Kuan <soonkwan21c@gmail.com>
2025-04-27 22:47:59 +0900

    25/4/27

--- bj2399.cpp
+++ bj2399.cpp
@@ -14,8 +14,24 @@
 #define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
 using namespace std;
 
+struct compMin{
+    bool operator() (const ll& a, const ll& b) const{
+        return a < b;
+    }
+};  
 void INPUT(){
+    int n; cin >> n;
+    vector<ll> v(n);
+    for (int i=0; i<n; i++) cin >> v[i];
+    sort(v.begin(), v.end(), compMin());
 
+    ll ans = 0;
+    for (int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            ans += abs(v[i] - v[j]);
+        }
+    }
+    cout << ans * 2 << endl;
 }
 void SOLUTION(){
 
