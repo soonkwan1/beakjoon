2b0c8570eda231baf1a2adc9863ecbbd4352fe1c
Kuan <soonkwan21c@gmail.com>
2025-08-14 22:48:27 +0900

    25/8/14

---
+++ bj32651.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+using namespace std;
+
+void INPUT(){
+    ll n; cin >> n;
+    if (n > 100000 || n % 2024 != 0) cout << "No" << endl;
+    else cout << "Yes" << endl;
+}
+void SOLUTION(){
+
+}
+int main(){
+    fastio;
+    INPUT();
+    SOLUTION();
+    return 0;
+}
