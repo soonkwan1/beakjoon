99c9296948275609365a24eb60fd91f159525cf1
Kuan <88532488+soonkwan1@users.noreply.github.com>
2025-09-26 14:41:26 +0900

    25/9/26

---
+++ bj17496.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+
+void INPUT() {
+    int n, t, c, p; cin >> n >> t >> c >> p;
+    n--;
+    cout << c * p * (n / t) << endl;
+}
+
+int main() {
+    fastio;
+    INPUT();
+    return 0;
+}
