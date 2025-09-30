d00a96e664dc9f4d5e3b7d70faa897e19a338259
Kuan <soonkwan21c@gmail.com>
2025-09-30 22:47:00 +0900

    25/9/30

--- bj5893.cpp
+++ bj5893.cpp
@@ -14,10 +14,43 @@
 #define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
 using namespace std;
 
+string binaryAdd(string a, string b){
+    int k = b.size();
+    for (int i=0; i<a.size() - k; i++){
+        b = "0" + b;
+    }
+
+    string ret = "";
+    int addon = 0;
+    for (int i=a.size() - 1; i>=0; i--){
+        int t1 = int(a[i]) - 48;
+        int t2 = int(b[i]) - 48;
+        int tmp = t1 + t2 + addon;
+        if (tmp == 0){
+            addon = 0;
+            ret = "0" + ret;
+        }
+        else if (tmp == 1){
+            addon = 0;
+            ret = "1" + ret;
+        }
+        else if (tmp == 2){
+            addon = 1;
+            ret = "0" + ret;
+        }
+        else if (tmp == 3){
+            addon = 1;
+            ret = "1" + ret;
+        }
+    }
+    if (addon) ret = "1" + ret;
+    return ret;
+}
+
 void INPUT(){
     string num; cin >> num;
-    string mul = "10001";
-    
+    string num2 = num + "0000";
+    cout << binaryAdd(num2, num) << endl;
 }
 void SOLUTION(){
     
