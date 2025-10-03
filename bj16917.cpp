ccc783cce2f002013b72ad6c52498af6120ebd46
Kuan <88532488+soonkwan1@users.noreply.github.com>
2025-10-03 21:26:37 +0900

    25/10/3

---
+++ bj16917.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+
+void INPUT() {
+    int A, B, C, X, Y;
+    cin >> A >> B >> C >> X >> Y;
+
+    bool ischeapBanBan = A + B > 2 * C ? true : false;
+    long long mini = 10000000000, sum;
+    if (ischeapBanBan) {
+        if (X > Y) {
+            for (int i = Y; i <= X; i++) {
+                sum = 2 * C * i + (X - i) * A;
+                if (sum < mini) { mini = sum; }
+            }
+        }
+        else {
+            for (int i = X; i <= Y; i++) {
+                sum = 2 * C * i + (Y - i) * B;
+                if (sum < mini) { mini = sum; }
+            }
+        }
+
+    }
+    else {
+        mini = A * X + B * Y;
+    }
+
+    cout << mini;
+}
+
+int main() {
+    fastio;
+    INPUT();
+    return 0;
+} 
