54fb8e97decb14014dda7a5e545c4bc7f9f49918
Kuan <soonkwan21c@gmail.com>
2025-10-28 22:15:00 +0900

    25/10/28

---
+++ bj31472.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+using namespace std;
+
+void INPUT(){
+    int w; cin >> w;
+    cout << sqrt(w * 2) * 4 << endl;
+}
+void SOLUTION(){
+
+}
+int main(){
+    fastio;
+    INPUT();
+    SOLUTION();
+    return 0;
+}
