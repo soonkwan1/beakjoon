049df7993c3b8a6cffdb5147449f0bc40c9e6826
Kuan <88532488+soonkwan1@users.noreply.github.com>
2026-01-09 20:22:52 +0900

    26/1/9

---
+++ bj11908.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+
+void INPUT() {
+    int n; cin >> n;
+    priority_queue<int> pq;
+    for (int i = 0; i < n; i++) {
+        int tmp; cin >> tmp;
+        pq.push(tmp);
+    }
+
+    int ans = 0;
+    pq.pop();
+    while (!pq.empty()) {
+        ans += pq.top();
+        pq.pop();
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    fastio;
+    INPUT();
+    return 0;
+} 
