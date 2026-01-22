7169f3bf4c1b4834070eee1f857441af64508875
Kuan <soonkwan21c@gmail.com>
2026-01-22 23:01:51 +0900

    26/1/22

---
+++ bj14624.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+using namespace std;
+
+void INPUT(){
+    int n; cin >> n;
+    if (n % 2 == 0) cout << "I LOVE CBNU" << endl;
+    else{
+        int start = n / 2;
+        int mid = 1;
+        for (int i=0; i<n; i++){
+            cout << "*";
+        }
+        cout << endl;
+        while (start >= 0){
+            for (int i=0; i<start; i++) cout << " ";
+            if (start == n / 2) cout << "*" << endl;
+            else{
+                cout << "*";
+                for (int i=0; i<mid; i++) cout << " ";
+                cout << "*" << endl;
+                mid += 2;
+            }
+            start--;
+        }
+    }
+}
+void SOLUTION(){
+
+}
+int main(){
+    fastio;
+    INPUT();
+    SOLUTION();
+    return 0;
+}
