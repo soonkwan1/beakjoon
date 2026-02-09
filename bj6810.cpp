21a2ac6ed54fa925f955f51fdc870cdad45007d8
Kuan <soonkwan21c@gmail.com>
2026-02-09 22:43:22 +0900

    26/2/9

--- bj6810.cpp
+++ bj6810.cpp
@@ -15,7 +15,23 @@
 using namespace std;
 
 void INPUT(){
-    
+    int list[13] = {9, 7, 8, 0, 9, 2, 1, 4, 1, 8};
+    cin >> list[10] >> list[11] >> list[12];
+
+    int state = 0;
+    int ans = 0;
+    for (int i=0; i<13; i++){
+        if (!state){
+            ans += list[i] * 1;
+            state = 1;
+        }
+        else if (state){
+            ans += list[i] * 3;
+            state = 0;
+        }
+    }
+    cout << "The 1-3-sum is ";
+    cout << ans << endl;
 }
 void SOLUTION(){
     
