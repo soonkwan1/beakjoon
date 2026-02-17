007006a683826a826da7b68e8585e8d7a49439df
Kuan <88532488+soonkwan1@users.noreply.github.com>
2026-02-17 12:50:06 +0900

    26/2/17

---
+++ bj6887.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+
+void INPUT() {
+    int n; cin >> n;
+    cout << "The largest square has side length " << int(sqrt(n)) << "." << endl;
+}
+
+int main() {
+    fastio;
+    INPUT();
+    return 0;
+} 
