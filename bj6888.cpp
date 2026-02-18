57f54219f5532f077ad5c6c9175f800fbcca8c16
Kuan <88532488+soonkwan1@users.noreply.github.com>
2026-02-18 16:52:48 +0900

    26/2/18

---
+++ bj6888.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+
+void INPUT() {
+    int x, y; cin >> x >> y;
+    for (int i = x; i <= y; i += 60) {
+        cout << "All positions change in year " << i << endl;
+    }
+}
+
+int main() {
+    fastio;
+    INPUT();
+    return 0;
+} 
