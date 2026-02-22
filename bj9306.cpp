8a33c736eb3eb0c9ecc93eeaf8e2ae97cd698b59
Kuan <soonkwan21c@gmail.com>
2026-02-22 20:35:03 +0900

    26/2/22

---
+++ bj9306.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+using namespace std;
+
+void INPUT(){
+    int n; cin >> n;
+    for (int i=1; i<=n; i++){
+        string s1, s2; cin >> s1 >> s2;
+        cout << "Case " << i << ": " << s2 << ", " << s1 << endl;
+    }
+}
+void SOLUTION(){
+    
+}
+
+int main(){
+    fastio;
+    INPUT();
+    SOLUTION();
+    return 0;
+}
