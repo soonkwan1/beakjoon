47e3cfb1468ce0a61d7ccd5422ce660f2e40358c
Kuan <88532488+soonkwan1@users.noreply.github.com>
2026-03-06 20:16:25 +0900

    26/3/6

---
+++ bj15610.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#define endl "\n"
+#define ll long long
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+using namespace std;
+
+
+void INPUT() {
+    unsigned ll tmp; cin >> tmp;
+    cout.precision(6);
+    cout << fixed;
+
+    cout << sqrt(tmp) * 4 << endl;
+}
+void SOLUTION() {
+
+}
+
+int main() {
+    fastio;
+    INPUT();
+    SOLUTION();
+    return 0;
+}
