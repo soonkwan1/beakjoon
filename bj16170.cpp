232076e1db170c86259497324ccb915cd907178b
Kuan <soonkwan21c@gmail.com>
2026-03-15 21:53:17 +0900

    26/3/15

---
+++ bj16170.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define endl "\n"
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+using namespace std;
+
+void INPUT(){
+    cout << "2026" << endl;
+    cout << "3" << endl;
+    cout << "15" << endl;
+}
+int main(){
+    fastio;
+    INPUT();
+    return 0;
+}
