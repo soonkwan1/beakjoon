6862114fc69f7a8705c502f7bde36f63aa0e4e5a
Kuan <88532488+soonkwan1@users.noreply.github.com>
2026-04-26 18:57:14 +0900

    26/4/26

---
+++ bj29863.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define INF 98765432101
+#define endl "\n"
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+
+void INPUT() {
+    int a, b; cin >> a >> b;
+    if (a <= 19) a += 24;
+    b += 24;
+    cout << b - a << endl;
+}
+
+int main() {
+    fastio;
+    INPUT();
+    return 0;
+} 
